75db0b84f9950de055907710f25ff1b5c88437f0
Mark <mark@murphymark.me>
2021-12-27 12:11:44 -0600

    cors test

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,32 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import { StoplightProject } from '@stoplight/elements-dev-portal';
+import '@stoplight/elements-dev-portal/styles.min.css';
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="tags/apps">
-          <h2 style={{ color: 'blue', cursor: 'pointer' }}>Apps</h2>
-        </Link>
-      </li>
-      <li>
-        <Link href="tags/webhooks">
-          <h2 style={{ color: 'blue', cursor: 'pointer' }}>Webhooks</h2>
-        </Link>
-      </li>
-      <li>
-        <Link href="guides">
-          <h2 style={{ color: 'blue', cursor: 'pointer' }}>guides</h2>
-        </Link>
-      </li>
-    </ul>
+    // <ul>
+    //   <li>
+    //     <Link href="tags/apps">
+    //       <h2 style={{ color: 'blue', cursor: 'pointer' }}>Apps</h2>
+    //     </Link>
+    //   </li>
+    //   <li>
+    //     <Link href="tags/webhooks">
+    //       <h2 style={{ color: 'blue', cursor: 'pointer' }}>Webhooks</h2>
+    //     </Link>
+    //   </li>
+    //   <li>
+    //     <Link href="guides">
+    //       <h2 style={{ color: 'blue', cursor: 'pointer' }}>guides</h2>
+    //     </Link>
+    //   </li>
+    // </ul>
+    <StoplightProject
+      projectId="cHJqOjIwNjAz"
+      router="memory"
+      // tryItCorsProxy="https://cors-anywhere-proxy-mm.herokuapp.com/"
+    />
   );
 }
